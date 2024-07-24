f9c8d1fef43c40e71a21fd6e1d2533a0edcf4d60
geoffrey0pv <geoffreypv00@gmail.com>
2024-07-24 17:22:37 -0500

    edit building & vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // El directorio de salida de los archivos estáticos
+  },
 })
